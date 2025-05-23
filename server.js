0beab13f58d6fbbf63570efc6d8eba5c2f9de654
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-05-23 18:37:15 +1000

    Setting up username and password validation

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 import express from "express";
 
 const app = express();
+// Middleware
+app.set("view engine", "ejs");
+app.use(function (req, res, next) {
+  res.locals.errors = [];
+  next();
+});
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("homepage");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/register", (req, res) => {
+  const errors = [];
+
+  if (typeof req.body.username !== "string") req.body.username = "";
+  if (typeof req.body.password !== "string") req.body.password = "";
+
+  req.body.username = req.body.username.trim();
+  if (!req.body.username) errors.push("You must enter a username");
+  if (req.body.username && req.body.username.length < 3)
+    errors.push("Username should be at least 3 characters");
+  if (req.body.username && req.body.username.length > 10) {
+    errors.push("Username should be less than 10 characters");
+  }
+  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))
+    errors.push("Username can only contain letters and numbers");
+
+  // password validation
+  req.body.password = req.body.password.trim();
+  if (!req.body.password) errors.push("You must enter a password");
+  if (req.body.password && req.body.password.length < 8)
+    errors.push("Password should be at least 8 characters");
+  if (req.body.password && req.body.password.length > 20) {
+    errors.push("Password should be less than 20 characters");
+  }
+  if (errors.length) {
+    return res.render("homepage", { errors });
+  } else {
+    res.send("Registered");
+  }
 });
 
 app.listen(3000);
